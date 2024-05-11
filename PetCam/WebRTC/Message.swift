8b73cacf361cd455537e0b8f86f2ddd748ba0036
Seok231 <diddbstjr55@gmail.com>
2024-05-11 14:11:37 +0900

    SRT -> WebRTC

---
+++ PetCam/WebRTC/Message.swift
@@ -0,0 +1,8 @@
+//
+//  Message.swift
+//  AtThisMoment
+//
+//  Created by 양윤석 on 5/6/24.
+//
+
+import Foundation
---
+++ PetCam/WebRTC/SignalingClient.swift
@@ -0,0 +1,8 @@
+//
+//  SignalingClient.swift
+//  AtThisMoment
+//
+//  Created by 양윤석 on 5/6/24.
+//
+
+import Foundation
